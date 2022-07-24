bc5e29c89a842d81f6c6247af4392ec3beb7ba81
Mahender Reddy <mahender@devappsol.com>
2022-07-24 19:19:55 +0530

    Added logging, docker compose volumes

--- src/main/java/com/mahender/webapp/docker/dockerdemo/DockerdemoApplication.java
+++ src/main/java/com/mahender/webapp/docker/dockerdemo/DockerdemoApplication.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 @RestController
 public class DockerdemoApplication {
 
+	private static final Logger logger = LogManager.getLogger(DockerdemoApplication.class);
+
 	@RequestMapping("/")
 	public String home() {
+		logger.info("Home called");
         return "Hello Docker World";
 	}
 
